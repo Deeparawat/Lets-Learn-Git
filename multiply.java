4766cd86750cebc88224865a9951fdc00347071d
Deepa Rawat <rawatdeepa334@gmail.com>
2023-04-19 10:46:45 +0530

    mul.java added

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main(String args[]) {
+        int a=6;
+        int b=10;
+        System.out.println("mul is : "+ (a*b));
+        
+        
+    }
+}
